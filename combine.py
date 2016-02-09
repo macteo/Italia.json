292f230d7df230adf52a4c0cdc045c43814350e1
Matteo Gavagnin <m@macteo.it>
2016-02-09 09:15:30 +0100

    Removed istat code and region/provice grouping from the output.

--- combine.py
+++ combine.py
@@ -7,41 +7,52 @@ italiajs = json.loads (open ('italia.json', 'r').read ())
 comcsv = open ('comuni.csv', 'r'). read (). split ('\n')
 
 # Creo una lista che associa comuni a provincia
-comlist = {}
+comlist = []
+index = 0
 for comline in comcsv:
 	com = comline.split (';')
 	
+	index = index + 1
+	
+	if index == 1:
+		continue
+	
 	if len (com) < 6:
 		continue;
-		
-	if not (com[2] in comlist):
-		comlist[com[2]] = []
-	comlist[com[2]].append ([ com[0], com[1], com[5] ])
+	
+    # if not (com[2] in comlist):
+    #     comlist[com[2]] = []
+	comlist.append ([ com[0], com[1], com[5] ])
 
 # Modifico italia.js in modo che contenga, per ogni provincia, anche i comuni
-for regione in italiajs["regioni"]:
-	newprov = []
-	for provincia in regione['province']:
-		prov = {}
-		prov['code'] = provincia
-		prov['nome'] = regione['capoluoghi'][regione['province'].index (provincia)]
-		prov['comuni'] = []
-		
-		for com in comlist[provincia]:
-			nc = {}
-			nc['nome'] = com[1].decode('latin1')
-			nc['cap'] = com[2]
-			nc['code'] = com[0]
-			prov['comuni'].append (nc)
-		
-		newprov.append (prov)
+comuni = []
+# for regione in italiajs["regioni"]:
+#     newprov = []
+#     for provincia in regione['province']:
+#         prov = {}
+#         prov['code'] = provincia
+#         prov['nome'] = regione['capoluoghi'][regione['province'].index (provincia)]
+#         prov['comuni'] = []
+#
+for com in comlist:
+	nc = {}
+	nc['nome'] = com[1].decode('latin1')
+	nc['cap'] = com[2]
+	#nc['code'] = com[0]
+	#prov['comuni'].append (nc)
+	comuni.append (nc)
+		# newprov.append (prov)
 	
-	del regione['capoluoghi']
-	regione['province'] = newprov
+	# del regione['capoluoghi']
+	# regione['province'] = newprov
+
+del italiajs["regioni"]
+
+italiajs = comuni
 
 
-out = open ('italia_comuni.json', 'w')
-json.dump(italiajs, out, indent=4)
+out = open ('comuni.json', 'w')
+json.dump(italiajs, out, indent=None,separators=(',', ':'))
 out.close ();
 
 
